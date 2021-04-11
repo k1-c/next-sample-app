d9190455823610804bf380ceb72f2e6a378da3cc
shunk-py <auau3700@gmail.com>
2021-04-11 14:55:30 +0900

    feat: add first post page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
@@ -6,12 +7,11 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">this page!</Link><a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
